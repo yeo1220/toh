868772fea8360564365c1c8419c0b8a370fc20fc
yeo1220 <yeo1220@gmail.com>
2018-03-10 15:43:07 +0900

    Tutorial 5
    Detail View 컴포넌트화

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,7 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
   hero: Hero = {
     id: 10000,
     name: 'Winstorm'
